f98bfcc389952df21356338021342fdd054fdca6
ESMA_MarieR <m.roy@rns.ecolescreatives.com>
2024-12-06 16:54:11 +0100

    push

--- Assets/Scripts/New Folder/ClickerFisher.cs
+++ Assets/Scripts/New Folder/ClickerFisher.cs
@@ -6,9 +6,16 @@ using TMPro;
 
 public class ClikerFish : MonoBehaviour
 {
+    //Texte score poisson
     public int score = 0;
-    [SerializeField]
+    [SerializeField]        
     public TMP_Text scoreText;
+
+
+    public int bonus1Cost = 10;
+    public int bonus2Cost = 10;
+    public int bonus3Cost = 10;
+    private int bonusMultiplier = 1;
 
 
     public void OnButtonClick()
@@ -17,6 +24,49 @@ public class ClikerFish : MonoBehaviour
         UpdateScoreText();
     }
 
+    public void BuyBonus1()
+    {
+        if (score >= bonus1Cost)
+        {
+            score -= bonus1Cost;
+            bonusMultiplier++;
+            UpdateScoreText();
+            Debug.Log("b1 acheté");
+        }
+        else
+        {
+            Debug.Log("pas assez)");
+        }
+    }
+    public void BuyBonus2()
+    {
+        if (score >= bonus2Cost)
+        {
+            score -= bonus2Cost;
+            bonusMultiplier += 2;
+            UpdateScoreText();
+            Debug.Log("b2 acheté");
+        }
+        else
+        {
+            Debug.Log("pas assez)");
+        }
+    }
+
+    public void BuyBonus3()
+    {
+        if (score >= bonus3Cost)
+        {
+            score -= bonus3Cost;
+            bonusMultiplier += 5;
+            UpdateScoreText();
+            Debug.Log("b3 acheté");
+        }
+        else
+        {
+            Debug.Log("pas assez)");
+        }
+    }
     void UpdateScoreText()
     {
         scoreText.text = score + " Poisson ";
